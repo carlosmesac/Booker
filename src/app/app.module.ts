b01a838832e34f8fbf72690110226b2d7bb1169c
Antonio <galvanantonio.alumno@gmail.com>
2020-10-04 17:00:44 +0100

    folders created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSelect, MatSelectModule } from '@angular/material/select';
+import {MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatOptionModule } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SharedComponent } from './shared/shared.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProfileComponent,
+    SharedComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
