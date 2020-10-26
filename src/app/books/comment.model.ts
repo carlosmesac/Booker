e0a24d7421d84fed1317e09e368045cb6b4df70f
Carlos <carlosmesac98@gmail.com>
2020-10-26 12:11:30 +0000

    Tarjetas de libros en profile

--- src/app/books/comment.model.ts
+++ src/app/books/comment.model.ts
@@ -1,15 +1,15 @@
 
 export class Comment {
   public thumbnail:string
-  public like:boolean
+  public liked:boolean
   public comment: string
   public title:string
   public publishDate:number
 
 
-    constructor(thumbnail:string,title:string, like:boolean ,comment: string){
+    constructor(thumbnail:string,title:string, liked:boolean ,comment: string){
       this.thumbnail = thumbnail
-      this.like = like
+      this.liked = liked
       this.title = title
       this.comment = comment
       this.publishDate = this.getCurrentDate()
